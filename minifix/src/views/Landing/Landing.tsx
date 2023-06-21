911514e1b5085f684f4c568aeb7d73b9353dfbb6
Josefine Forsman <josefineforsman1@gmail.com>
2023-06-21 22:50:39 +0200

    mobile version landing page

---
+++ minifix/src/views/Landing/Landing.tsx
@@ -0,0 +1,23 @@
+import './Landing.css';
+import { useNavigate } from 'react-router';
+
+function Landing(){
+    const navigate = useNavigate();
+
+    function navigateToProducts(){
+        navigate('/produkter')
+    }
+
+    return(
+        <section className='landing'>
+            <article className='landing__text'>
+                <h1 className='text__title'>Minfix</h1>
+                <h3 className='text__sub-title'>Välkommen hit!</h3>
+                <p className='text__body'>Letar du efter unika barnkläder som funkar lika bra till både tjej och kille? Då har du kommit rätt!</p>
+            </article>
+        <button className='button__primary' onClick={navigateToProducts}>Se alla produkter</button>
+        </section>
+    )
+}
+
+export default Landing;
---
+++ minifix/src/views/Products/Products.tsx
@@ -0,0 +1,8 @@
+import './Products.css';
+
+function Products(){
+    return(
+        <section>Products</section>
+    )
+}
+export default Products;
