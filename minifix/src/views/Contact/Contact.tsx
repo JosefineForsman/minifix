1312bc958a5d2b42307a1571b4d85f164f37e033
Josefine Forsman <josefineforsman1@gmail.com>
2023-07-06 23:11:01 +0200

    bort med design. svart + vitt

--- minifix/src/views/Contact/Contact.tsx
+++ minifix/src/views/Contact/Contact.tsx
@@ -1,11 +1,38 @@
+import Header from '../../components/Header/Header';
 import './Contact.css';
 
 function Contact(){
 
     return(
-        <section>
-            Kontakta oss
+
+    <div className="hero">
+        <section className='form-container'>
+            <Header/>
         </section>
+          <form name="company-form" action="form_cont.html" method="post" >
+            <h1 className='text__title'>Kontakta oss :</h1>
+            <h2 className='text__form'>Skulle det vara så att du har några som helst frågor så är det bara att skicka iväg ett meddelande här!</h2>
+            <br/>
+          <div className="form1">
+            <input className='input' type="text" required/>
+            <label htmlFor="fname" className='text__body'> Namn: </label>
+          </div>
+          <div className="form1">
+            <input className='input'type="number" required/>
+            <label htmlFor="number" className='text__body'> Telefon nummer: </label>
+          </div>
+          <div className="form1">
+            <input className='input' type="email" required/>
+            <label htmlFor="email" className='text__body'> Email adress:</label>
+          </div>
+          <div className="form1">
+            <p className='text__body'> Ditt meddelande : </p>
+            <textarea id="message"></textarea>
+            <label htmlFor="message"></label>
+          </div>
+          <button type="submit" className='button__primary button__primary-sumbit'>Skicka </button>     
+          </form>
+          </div>
     )
 }
 export default Contact;
--- minifix/src/views/Landing/Landing.tsx
+++ minifix/src/views/Landing/Landing.tsx
@@ -1,3 +1,4 @@
+import Header from '../../components/Header/Header';
 import Nav from '../../components/Nav/Nav';
 import './Landing.css';
 import { useNavigate } from 'react-router';
@@ -12,8 +13,9 @@ function Landing(){
     return(
         <section className='landing'>
             <article className='landing__text'>
-                <h1 className='text__title'>Minfix</h1>
+                <Header/>
                 <h3 className='text__sub-title'>Välkommen hit!</h3>
+                <p>bildspel här? eller bilder?</p>
                 <p className='text__body'>Letar du efter unika barnkläder som funkar lika bra till både tjej och kille? Då har du kommit rätt!</p>
             </article>
         <button className='button__primary' onClick={navigateToProducts}>Se alla produkter</button>
