4937de8564c08f206ab3910b5b0a4ee346927e21
Josefine Forsman <josefineforsman1@gmail.com>
2023-07-06 23:12:16 +0200

    -

--- minifix/src/views/AboutUs/AboutUs.tsx
+++ minifix/src/views/AboutUs/AboutUs.tsx
@@ -8,7 +8,7 @@ function AboutUs(){
             <Header/>
             <h1 className="text__title">Om oss, vilka är vi?</h1>
             <p className="text__body">Välkommen till Minifix, där vi skapar handgjorda barn- och babyprodukter med omsorg och kärlek. Vi tror på att erbjuda produkter av högsta kvalitet som är både vackra och praktiska för ditt barns vardag.</p>
-            <p className="text__body">Vårt företag grundades med en passion för att erbjuda barnkläder och accessoarer som är tidlösa och kan bäras av alla barn oavsett kön. Vi strävar efter att bryta stereotyper och främja ett unisex perspektiv på barnkläder, vilket gör att våra produkter blir användbara och kan ärvas vidare till syskon och längre.</p>
+            <p className="text__body">Minifix grundades med en passion för att erbjuda barnkläder och accessoarer som är tidlösa och kan bäras av alla barn oavsett kön. Vi strävar efter att bryta stereotyper och främja ett unisex perspektiv på barnkläder, vilket gör att våra produkter blir användbara och kan ärvas vidare till syskon och längre.</p>
             <p className="text__body">Varje plagg sys för hand med stor omsorg och uppmärksamhet på detaljer. Vi använder noggrant utvalda material av högsta kvalitet för att säkerställa att våra produkter är skonsamma mot ditt barns känsliga hud och samtidigt hållbara för långvarig användning.</p>
             <p className="text__body">Vi är stolta över att vara en del av ditt barns resa genom barndomen och att skapa plagg som kan följa dem under lång tid. Hos Minifix är vi dedikerade till att erbjuda hållbara och tidlösa barn- och babyprodukter som står emot trender och kan vara en del av familjens minnen och arv.</p>
         </section>
